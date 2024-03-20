bdec4fdbb9d24d2ce41868d9d7caeae9c93ec636
Luis Henrique Porto Menezes <128551788+luishmenezes@users.noreply.github.com>
2024-03-20 13:26:26 -0300

    Add files via upload

---
+++ DOM com js/index.js
@@ -0,0 +1,35 @@
+const inputElement = document.querySelector("input");
+const buttonElement = document.querySelector("form button");
+const ulElement = document.querySelector("ul");
+
+
+
+
+buttonElement.onclick = adiciona => {
+    adiciona.preventDefault();
+
+
+    if (inputElement.value){
+        const textElement = document.createElement("span");
+        textElement.innerHTML = inputElement.value;
+
+        const btnElement = document.createElement("button");
+        btnElement.innerHTML = "Remover";
+
+        const liElement = document.createElement("li");
+        liElement.appendChild(textElement);
+        liElement.appendChild(btnElement);
+
+        btnElement.onclick = () => {
+            ulElement.removeChild(liElement);
+        }
+
+
+        ulElement.appendChild(liElement);
+
+        inputElement.value = "";
+        
+        
+    }
+    
+}
