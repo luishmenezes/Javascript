8c7b0430f9765fde3cbc5eae694f2df1de924196
Luis Henrique Porto Menezes <128551788+luishmenezes@users.noreply.github.com>
2024-03-23 15:37:37 -0300

    Add files via upload

---
+++ atv5/q4.js
@@ -0,0 +1,9 @@
+function caixaAlta(string){
+    return string.toUpperCase();
+}
+
+var string1 = "luis, aumentando a letra";
+var string2 = "milena, aumentando a letra";
+
+console.log(caixaAlta(string1));
+console.log(caixaAlta(string2));
